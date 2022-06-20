921873cb027af5bf9f603ad21fba8446b7d49efb
Archie David <archiedavid@users.noreply.github.com>
2022-06-20 21:09:57 +0100

    better example

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,8 +1,13 @@
 package com.example.plugins
 
-import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.nimbusds.jose.JOSEObjectType
+import com.nimbusds.jose.JWSAlgorithm
+import com.nimbusds.jose.JWSHeader
 import com.nimbusds.jose.JWSObject
+import com.nimbusds.jose.JWSSigner
 import com.nimbusds.jose.Payload
+import com.nimbusds.jose.crypto.RSASSASigner
 import com.nimbusds.jose.crypto.RSASSAVerifier
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
@@ -14,15 +19,21 @@ import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
 import java.security.KeyFactory
+import java.security.interfaces.RSAPrivateKey
 import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
 import java.util.Base64
 
 fun Application.configureRouting() {
 
     val RSA_PUBLIC_KEY_STR = """-----BEGIN PUBLIC KEY-----
+    removed!
+    -----END PUBLIC KEY-----""".trimIndent()
 
------END PUBLIC KEY-----"""
+    val RSA_PRIVATE_KEY_STR = """-----BEGIN PRIVATE KEY-----
+    removed!
+    -----END PRIVATE KEY-----""".trimIndent()
 
     val keyFactory = KeyFactory.getInstance("RSA")
 
@@ -35,16 +46,29 @@ fun Application.configureRouting() {
     ) as RSAPublicKey
 
 
-    // Starting point for a Ktor app:
+    val RSA_PRIVATE_KEY = keyFactory.generatePrivate(
+        PKCS8EncodedKeySpec(
+            Base64.getDecoder().decode(
+                removeEncapsulationBoundaries(RSA_PRIVATE_KEY_STR)
+            )
+        )
+    ) as RSAPrivateKey
+
+
     routing {
         post("/validatesignature") {
-            val jsonPayloadStr = call.receive<String>()
+            val person = call.receive<Person>()
             val xJwsSignature = call.request.header("x-jws-signature")
 
-            println("jsonPayloadStr : " + jsonPayloadStr)
+            println("jsonPayloadStr : " + person)
             println("xJwsSignature : " + xJwsSignature)
 
-            val jwsObject = JWSObject.parse(xJwsSignature, Payload(jsonPayloadStr))
+            val personJson = ObjectMapper().writeValueAsString(person)
+            println("personJson = $personJson")
+
+            val payload = Payload(personJson)
+
+            val jwsObject = JWSObject.parse(xJwsSignature, payload)
             println("JWEOBJECT: " + jwsObject.toString())
             val verifier = RSASSAVerifier(RSA_PUBLIC_KEY as RSAPublicKey)
 
@@ -60,6 +84,36 @@ fun Application.configureRouting() {
             }
         }
 
+
+        post("/sign") {
+            try {
+                val person = call.receive<Person>()
+                println("person = $person")
+
+                val personJson = ObjectMapper().writeValueAsString(person)
+                println("personJson = $personJson")
+
+                val payload = Payload(personJson)
+                val jwsObject = JWSObject(
+                    JWSHeader.Builder(JWSAlgorithm.RS256).type(JOSEObjectType.JWT).build(),
+                    payload
+                )
+
+                val signer: JWSSigner = RSASSASigner(RSA_PRIVATE_KEY)
+
+                jwsObject.sign(signer)
+                val isDetached = true
+                val signature = jwsObject.serialize(isDetached)
+
+                println("signature = " + signature)
+                call.respond(HttpStatusCode.OK, "created signature =  $signature")
+            } catch (t: Throwable) {
+                println(t)
+                call.respond(HttpStatusCode.InternalServerError)
+            }
+
+        }
+
         get("/test") {
             call.respond(HttpStatusCode.OK, "hello world")
         }
@@ -68,9 +122,11 @@ fun Application.configureRouting() {
     }
 }
 
-
 private fun removeEncapsulationBoundaries(key: String): String {
     return key.replace("\n".toRegex(), "")
         .replace(" ".toRegex(), "")
         .replace("-{5}[a-zA-Z]*-{5}".toRegex(), "");
 }
+
+
+data class Person(val name: String, val age: Int)
